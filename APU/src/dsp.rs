1bed0b4604168ad57a24527b616aba8f41f8a8e7
tibaud le maout <tibaud.le-maout@epitech.eu>
2025-04-26 15:41:49 +0200

    :wrench: Fix wrong register for volume0_left

--- APU/src/dsp.rs
+++ APU/src/dsp.rs
@@ -1,7 +1,7 @@
 pub struct Dsp {
     pub regs: [u8; 128], // 128 DSP registers
-    pub selected: u8,    // currently selected register
-    
+    pub selected: u8,    // Currently selected register
+
     // Voice 0 state:
     pub voice0_pitch: u16,
     pub voice0_volume_left: u8,
@@ -11,7 +11,7 @@ pub struct Dsp {
 
 impl Dsp {
     pub fn new() -> Self {
-        Dsp {
+        Self {
             regs: [0; 128],
             selected: 0,
             voice0_pitch: 0,
@@ -22,33 +22,41 @@ impl Dsp {
     }
 
     pub fn set_register_select(&mut self, reg: u8) {
-        self.selected = reg & 0x7F; // only lower 7 bits are valid
+        self.selected = reg & 0x7F;
     }
 
     pub fn write_selected_register(&mut self, value: u8) {
         println!("DSP: Wrote 0x{:02X} to register 0x{:02X}", value, self.selected);
-    
         self.regs[self.selected as usize] = value;
-    
+
         match self.selected {
-            0x2C => {
+            0x2C => { // V0_PITCHL
                 println!("Setting Voice 0 pitch low byte to 0x{:02X}", value);
                 self.voice0_pitch = (self.voice0_pitch & 0xFF00) | (value as u16);
             }
-            0x2D => {
+            0x2D => { // V0_PITCHH
                 println!("Setting Voice 0 pitch high byte to 0x{:02X}", value);
                 self.voice0_pitch = (self.voice0_pitch & 0x00FF) | ((value as u16) << 8);
             }
-            0x4C => {
+            0x0C => { // V0_VOL_LEFT
                 println!("Setting Voice 0 volume left to 0x{:02X}", value);
                 self.voice0_volume_left = value;
             }
-            0x5C => {
+            0x1C => { // V0_VOL_RIGHT
                 println!("Setting Voice 0 volume right to 0x{:02X}", value);
                 self.voice0_volume_right = value;
             }
-            _ => { /* no special action */ }
+            0x4C => { // KEYON
+                println!("Setting KEYON to 0x{:02X}", value);
+                self.voice0_key_on = value & 0x01 != 0;
+            }
+            _ => { /* No special action */ }
         }
+
+        println!(
+            "Voice 0 state: pitch {}, volume L {}, volume R {}, key_on {}",
+            self.voice0_pitch, self.voice0_volume_left, self.voice0_volume_right, self.voice0_key_on
+        );
     }
 
     pub fn read_selected_register(&self) -> u8 {
@@ -57,19 +65,11 @@ impl Dsp {
         value
     }
 
-    pub fn step(&mut self) {
-        let keyon = self.regs[0x4C]; // KEYON register
-        if keyon & 0x01 != 0 { // voice 0
-            let pitch_low = self.regs[0x02]; // V0_PITCHL
-            let pitch_high = self.regs[0x03]; // V0_PITCHH
-            let pitch = ((pitch_high as u16) << 8) | (pitch_low as u16);
-    
-            let vol_left = self.regs[0x0C]; // V0_VOL_LEFT
-            let vol_right = self.regs[0x1C]; // V0_VOL_RIGHT
-    
+    pub fn step(&self) {
+        if self.voice0_key_on {
             println!(
                 "DSP: Voice 0 playing. Pitch: {}, Volume L: {}, Volume R: {}",
-                pitch, vol_left, vol_right
+                self.voice0_pitch, self.voice0_volume_left, self.voice0_volume_right
             );
         }
     }
--- APU/src/main.rs
+++ APU/src/main.rs
@@ -11,53 +11,58 @@ fn main()
     cpu.write_byte(0xffff, 0x00);
 
     // Program:
-    cpu.write_byte(0x0000, 0xE8); // MOV A, #0x2C
+    cpu.write_byte(0x0000, 0xE8); // MOV A, #0x2C  (V0_PITCHL select)
     cpu.write_byte(0x0001, 0x2C);
     cpu.write_byte(0x0002, 0xC4); // MOV ($F2), A
     cpu.write_byte(0x0003, 0xF2);
 
     // Set low pitch
-    cpu.write_byte(0x0004, 0xE8); // MOV A, #0x00
+    cpu.write_byte(0x0004, 0xE8); // MOV A, #0x00 (low byte)
     cpu.write_byte(0x0005, 0x00);
     cpu.write_byte(0x0006, 0xC4); // MOV ($F3), A
     cpu.write_byte(0x0007, 0xF3);
 
-    // Set high pitch
-    cpu.write_byte(0x0008, 0xE8); // MOV A, #0x2D
+    // Select high pitch
+    cpu.write_byte(0x0008, 0xE8); // MOV A, #0x2D (V0_PITCHH select)
     cpu.write_byte(0x0009, 0x2D);
     cpu.write_byte(0x000A, 0xC4); // MOV ($F2), A
     cpu.write_byte(0x000B, 0xF2);
-    cpu.write_byte(0x000C, 0xE8); // MOV A, #0x10
+
+    // Set high pitch
+    cpu.write_byte(0x000C, 0xE8); // MOV A, #0x10 (high byte)
     cpu.write_byte(0x000D, 0x10);
     cpu.write_byte(0x000E, 0xC4); // MOV ($F3), A
     cpu.write_byte(0x000F, 0xF3);
 
     // Set left volume
-    cpu.write_byte(0x0010, 0xE8); // MOV A, #0x4C
-    cpu.write_byte(0x0011, 0x4C);
+    cpu.write_byte(0x0010, 0xE8); // MOV A, #0x0C (V0_VOL_LEFT select)
+    cpu.write_byte(0x0011, 0x0C);
     cpu.write_byte(0x0012, 0xC4); // MOV ($F2), A
     cpu.write_byte(0x0013, 0xF2);
-    cpu.write_byte(0x0014, 0xE8); // MOV A, #0x7F
+
+    cpu.write_byte(0x0014, 0xE8); // MOV A, #0x7F (max volume)
     cpu.write_byte(0x0015, 0x7F);
     cpu.write_byte(0x0016, 0xC4); // MOV ($F3), A
     cpu.write_byte(0x0017, 0xF3);
 
     // Set right volume
-    cpu.write_byte(0x0018, 0xE8); // MOV A, #0x5C
-    cpu.write_byte(0x0019, 0x5C);
+    cpu.write_byte(0x0018, 0xE8); // MOV A, #0x1C (V0_VOL_RIGHT select)
+    cpu.write_byte(0x0019, 0x1C);
     cpu.write_byte(0x001A, 0xC4); // MOV ($F2), A
     cpu.write_byte(0x001B, 0xF2);
-    cpu.write_byte(0x001C, 0xE8); // MOV A, #0x7F
+
+    cpu.write_byte(0x001C, 0xE8); // MOV A, #0x7F (max volume)
     cpu.write_byte(0x001D, 0x7F);
     cpu.write_byte(0x001E, 0xC4); // MOV ($F3), A
     cpu.write_byte(0x001F, 0xF3);
 
     // KEY ON voice 0
-    cpu.write_byte(0x0020, 0xE8); // MOV A, #0x4C
+    cpu.write_byte(0x0020, 0xE8); // MOV A, #0x4C (KEYON select)
     cpu.write_byte(0x0021, 0x4C);
     cpu.write_byte(0x0022, 0xC4); // MOV ($F2), A
     cpu.write_byte(0x0023, 0xF2);
-    cpu.write_byte(0x0024, 0xE8); // MOV A, #0x01
+
+    cpu.write_byte(0x0024, 0xE8); // MOV A, #0x01 (bit 0 = voice 0 ON)
     cpu.write_byte(0x0025, 0x01);
     cpu.write_byte(0x0026, 0xC4); // MOV ($F3), A
     cpu.write_byte(0x0027, 0xF3);
