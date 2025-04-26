d230be5d47802540c0174c28c61a7a6adb390f58
C0Florent <114689807+C0Florent@users.noreply.github.com>
2025-04-26 12:16:50 +0200

    :snowflake: Add basic flake for rust project (#5)

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    rust-nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        rust-pkgs = import rust-nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with rust-pkgs; [
+            rustc
+            cargo
+            cargo-expand
+            rustfmt
+          ] ++ (with pkgs; [
+            hello # replace with actual packages when needed
+          ]);
+        };
+      });
+}
