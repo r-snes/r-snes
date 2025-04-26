260273c225b6f5c4df9661ea073ce7dac2758801
Clément Baziret <clement.baziret@epitech.eu>
2025-04-26 15:55:52 +0200

    :sparkles: Add a deduction of HiRom or LoRom

--- memory_map/src/rom.rs
+++ memory_map/src/rom.rs
@@ -3,7 +3,7 @@ use std::io::{self, Read};
 use std::path::Path;
 
 pub struct Rom {
-    data: Vec<u8>,
+    pub data: Vec<u8>,
 }
 
 impl Rom {
@@ -64,4 +64,60 @@ impl Rom {
     pub fn read_byte(&self, offset: usize) -> Option<u8> {
         self.data.get(offset).copied()
     }
+}
+
+pub enum RomMapping {
+    LoRom,
+    HiRom,
+    Unknown,
+}
+
+pub fn detect_rom_mapping(rom_data: &[u8]) -> RomMapping {
+    if rom_data.len() < 0x10000 {
+        return RomMapping::Unknown;
+    }
+
+    // Try LoROM header at 0x7FC0
+    let lorom_score = score_header(rom_data, 0x7FC0);
+    // Try HiROM header at 0xFFC0
+    let hirom_score = score_header(rom_data, 0xFFC0);
+
+    if lorom_score > hirom_score {
+        RomMapping::LoRom
+    } else if hirom_score > lorom_score {
+        RomMapping::HiRom
+    } else {
+        RomMapping::Unknown
+    }
+}
+
+fn score_header(rom_data: &[u8], header_offset: usize) -> u32 {
+    if header_offset + 0x20 > rom_data.len() {
+        return 0;
+    }
+
+    let mut score = 0;
+
+    // Title should be mostly ASCII
+    let title = &rom_data[header_offset..header_offset + 21];
+    if title
+        .iter()
+        .all(|&c| (c == 0x20) || (0x20..=0x7E).contains(&c))
+    {
+        score += 1;
+    }
+
+    // Checksum and complement
+    let checksum = read_u16(rom_data, header_offset + 0x1E);
+    let checksum_complement = read_u16(rom_data, header_offset + 0x1C);
+
+    if checksum != 0 && (checksum ^ checksum_complement) == 0xFFFF {
+        score += 2;
+    }
+
+    score
+}
+
+fn read_u16(data: &[u8], offset: usize) -> u16 {
+    (data[offset] as u16) | ((data[offset + 1] as u16) << 8)
 }
