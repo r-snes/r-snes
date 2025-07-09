ff312b2f9d291034ae43ab6207d762f239b9e309
Florent Charpentier <florent.charpentier@epitech.eu>
2025-07-09 20:50:16 +0200

    :adhesive_bandage: fix tests

--- cpu/src/lib.rs
+++ cpu/src/lib.rs
@@ -1,22 +1,13 @@
 mod registers;
-use registers::Registers;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use registers::Registers;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 
     #[test]
     fn regs () {
-        let regs = Registers {};
+        let regs = Registers::default();
     }
 }
