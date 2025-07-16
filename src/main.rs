25fa09c7469ac539981bad85bc29a1c3b1ca4b6d
Clément Baziret <clement.baziret@epitech.eu>
2025-07-16 19:58:32 +0200

    :construction_worker: Add cargo-tarpaulin to measure code coverage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
+    println!("{}", add(1, 2));
     println!("Hello, world!");
+}
+
+fn add(nb1: u16, nb2: u16) -> u16 {
+    nb1 + nb2
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
 }
