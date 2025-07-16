45769045fbb924f42a34cf534f56215522651b07
Clément Baziret <clement.baziret@epitech.eu>
2025-07-16 20:06:53 +0200

    :snowflake: Add pkgconfig and openssl in flake.nix pkgs

--- flake.nix
+++ flake.nix
@@ -5,27 +5,35 @@
     rust-nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         rust-pkgs = import rust-nixpkgs { inherit system; };
-      in {
+      in
+      {
         devShell = pkgs.mkShell {
-          buildInputs = with rust-pkgs; [
-            rustc
-            cargo
-            cargo-expand
-            rustfmt
-          ] ++ (with pkgs; [
-            hello # replace with actual packages when needed
-          ]);
+          buildInputs =
+            with rust-pkgs;
+            [
+              rustc
+              cargo
+              cargo-expand
+              rustfmt
+            ]
+            ++ (with pkgs; [
+              pkg-config
+              openssl
+            ]);
         };
-      });
+      }
+    );
 }
