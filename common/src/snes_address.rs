96da6cc7389bc06702bee5280e36190b833c5e39
Florent Charpentier <florent.charpentier@epitech.eu>
2025-09-14 03:58:40 +1000

    :sparkles: common: Add Default trait to SnesAddress

--- common/src/snes_address.rs
+++ common/src/snes_address.rs
@@ -2,7 +2,7 @@
 /// SNES adddress space.
 ///
 /// The address space is split in 256 64Ko banks.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
 pub struct SnesAddress {
     /// The bank number of the address
     pub bank: u8,
