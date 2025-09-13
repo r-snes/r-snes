61f43bb4c26da199e594a50b3bdce4006a9dc2ea
Clément Baziret <clement.baziret@epitech.eu>
2025-09-13 12:25:44 +0200

    :sparkles: Add the io in memory_mapping

--- bus/src/bus.rs
+++ bus/src/bus.rs
@@ -1,3 +1,4 @@
+use crate::io::Io;
 use crate::memory_region::MemoryRegion;
 use crate::rom::Rom;
 use crate::wram::Wram;
@@ -7,14 +8,16 @@ use std::path::Path;
 pub struct Bus {
     pub wram: Wram,
     pub rom: Rom,
+    pub io: Io,
     // TODO : Add other peripherals here later
 }
 
 impl Bus {
     pub fn new<P: AsRef<Path>>(rom_path: P) -> Result<Self, Box<dyn Error>> {
         Ok(Self {
-            wram: Wram::new(),
             rom: Rom::load_from_file(rom_path)?,
+            wram: Wram::new(),
+            io: Io::new(),
         })
     }
 
@@ -25,16 +28,20 @@ impl Bus {
         match bank {
             0x00..=0x3F | 0x80..=0xBF => {
                 if offset < 0x2000 {
-                    self.wram.read(addr & 0x1FFFF)
+                    self.wram.read(addr)
+                } else if offset >= 0x2000 && offset < 0x6000 {
+                    self.io.read(addr)
+                } else if offset >= 0x6000 && offset < 0x8000 {
+                    self.rom.read(addr) // TODO : Expansion port
                 } else if offset >= 0x8000 {
                     self.rom.read(addr)
                 } else {
-                    0xFF // TODO : Add other memory regions
+                    0xFF // TODO : Shouldn't come here, maybe just add debug ?
                 }
             }
             0x7E..=0x7F => self.wram.read(addr),
             0x40..=0x7D | 0xC0..=0xFF => self.rom.read(addr),
-            _ => 0xFF,
+            _ => 0xFF, // TODO : Shouldn't come here, maybe just add debug ?
         }
     }
 
@@ -45,13 +52,20 @@ impl Bus {
         match bank {
             0x00..=0x3F | 0x80..=0xBF => {
                 if offset < 0x2000 {
-                    self.wram.write((addr & 0x1FFFF), value);
+                    self.wram.write(addr, value);
+                } else if offset >= 0x2000 && offset < 0x6000 {
+                    self.io.write(addr, value);
+                } else if offset >= 0x6000 && offset < 0x8000 {
+                    self.rom.write(addr, value); // TODO : Expansion port
+                } else if offset >= 0x8000 {
+                    self.rom.write(addr, value); // ROM no writes handled in `rom`
                 } else {
-                    // TODO : Add other memory regions
+                    // TODO : Shouldn't come here, maybe just add debug ?
                 }
             }
             0x7E..=0x7F => self.wram.write(addr, value),
-            _ => {}
+            0x40..=0x7D | 0xC0..=0xFF => self.rom.write(addr, value),
+            _ => {} // TODO : Shouldn't come here, maybe just add debug ?
         }
     }
 }
