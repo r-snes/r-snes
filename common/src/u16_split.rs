d25fb1dfb4feacebeb40e1f0829f3d59bccbf0e1
Florent Charpentier <florent.charpentier@epitech.eu>
2025-10-04 17:57:23 +1000

    :memo: u16_split: Add doc comments

--- common/src/u16_split.rs
+++ common/src/u16_split.rs
@@ -1,28 +1,49 @@
+/// Extension trait for [`u16`] which defines additional methods
+/// allowing to "split" references (mutable or not) to a [`u16`] into
+/// two separate references to its individual bytes (as [`u8`]s)
 pub trait U16Split {
+    /// Split an immutable reference to a [`u16`] into two
+    /// immutable references to its bytes (as [`u8`]s)
     fn split<'a>(&'a self) -> SplitU16<'a>;
+    /// Split a mutable reference to a [`u16`] into two
+    /// mutable references to its bytes (as [`u8`]s)
     fn split_mut<'a>(&'a mut self) -> SplitU16Mut<'a>;
 
+    /// Get an immutable reference to the least significant byte of a [`u16`]
     fn lo<'a>(&'a self) -> &'a u8 {
         self.split().lo
     }
+    /// Get an immutable reference to the most significant byte of a [`u16`]
     fn hi<'a>(&'a self) -> &'a u8 {
         self.split().hi
     }
 
+    /// Get an mutable reference to the least significant byte of a [`u16`]
     fn lo_mut<'a>(&'a mut self) -> &'a mut u8 {
         self.split_mut().lo
     }
+    /// Get an mutable reference to the most significant byte of a [`u16`]
     fn hi_mut<'a>(&'a mut self) -> &'a mut u8 {
         self.split_mut().hi
     }
 }
 
+/// Helper struct which contains two immutable references
+/// to the individual bytes of a [`u16`]
 pub struct SplitU16<'a> {
+    /// Ref to the least signicant byte
     pub lo: &'a u8,
+    /// Ref to the most significant byte
     pub hi: &'a u8,
+
 }
+
+/// Helper struct which contains two immutable references
+/// to the individual bytes of a [`u16`]
 pub struct SplitU16Mut<'a> {
+    /// Mut ref to the least significant byte
     pub lo: &'a mut u8,
+    /// Mut ref to the most significant byte
     pub hi: &'a mut u8,
 }
 
