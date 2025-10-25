a26e65feb721759365c11825e7dd58176dc49c9b
Clément Baziret <clement.baziret@epitech.eu>
2025-10-25 16:14:47 +0200

    :fire: Remove useless warning dead code allow

--- bus/src/memory_region.rs
+++ bus/src/memory_region.rs
@@ -1,9 +1,7 @@
 use common::snes_address::SnesAddress;
 
 pub trait MemoryRegion {
-    #[allow(dead_code)]
     fn read(&self, addr: SnesAddress) -> u8;
 
-    #[allow(dead_code)]
     fn write(&mut self, addr: SnesAddress, value: u8);
 }
