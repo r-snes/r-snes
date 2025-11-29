855afa52c25d22b8d138e1d1f2d5fc4a262ffbf1
Clément Baziret <clement.baziret@epitech.eu>
2025-11-29 14:54:46 +0100

    :sparkles: Add framebuffer picture management

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,38 @@ fn main() -> eframe::Result {
 
 struct MyApp {
     pub rom_path: Option<PathBuf>,
+
+    // Raw framebuffer for now, waiting for ppu link
+    framebuffer: Vec<u8>,
+    fb_width: usize,
+    fb_height: usize,
+    fb_texture: Option<egui::TextureHandle>,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
-        Self { rom_path: None }
+        let fb_width = 256;
+        let fb_height = 224;
+
+        // Dummy framebuffer with a gradient
+        let mut framebuffer = vec![0u8; fb_width * fb_height * 4];
+        for y in 0..fb_height {
+            for x in 0..fb_width {
+                let i = (y * fb_width + x) * 4;
+                framebuffer[i] = x as u8; // R
+                framebuffer[i + 1] = y as u8; // G
+                framebuffer[i + 2] = 50; // B
+                framebuffer[i + 3] = 255; // A
+            }
+        }
+
+        Self {
+            rom_path: None,
+            framebuffer,
+            fb_width,
+            fb_height,
+            fb_texture: None,
+        }
     }
 }
 
@@ -68,6 +95,45 @@ impl MyApp {
     fn central_panel(&mut self, ctx: &egui::Context) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("R-SNES");
+
+            ui.separator();
+
+            ui.label("Loaded ROM:");
+
+            match &self.rom_path {
+                Some(path) => ui.label(path.display().to_string()),
+                None => ui.label("No ROM loaded"),
+            };
+
+            // Create or update texture
+            if self.fb_texture.is_none() {
+                // First time: allocate the texture
+                let img = egui::ColorImage::from_rgba_unmultiplied(
+                    [self.fb_width, self.fb_height],
+                    &self.framebuffer,
+                );
+
+                self.fb_texture =
+                    Some(ctx.load_texture("framebuffer", img, egui::TextureOptions::NEAREST));
+            } else {
+                // Update existing texture
+                if let Some(tex) = self.fb_texture.as_mut() {
+                    let img = egui::ColorImage::from_rgba_unmultiplied(
+                        [self.fb_width, self.fb_height],
+                        &self.framebuffer,
+                    );
+                    tex.set(img, egui::TextureOptions::NEAREST);
+                }
+            }
+
+            // Display framebuffer
+            if let Some(tex) = &self.fb_texture {
+                let desired_size = egui::vec2(
+                    self.fb_width as f32 * 2.0, // 2x scale
+                    self.fb_height as f32 * 2.0,
+                );
+                ui.image((tex.id(), desired_size));
+            }
         });
     }
 }
