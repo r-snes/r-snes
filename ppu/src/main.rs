b2106ea7133d16cac3f87acb0e8f10687c9bb488
Clément Baziret <clement.baziret@epitech.eu>
2026-02-07 17:31:24 +0100

    :rewind: Revert ppu-related file changes

--- ppu/src/main.rs
+++ ppu/src/main.rs
@@ -5,26 +5,26 @@ mod utils;
 use crate::ppu::PPU;
 use crate::tile::{load_and_split_image, load_tiles_into_vram};
 use crate::utils::{HEIGHT, SCREEN_HEIGHT, SCREEN_WIDTH, TILE_SIZE, WIDTH};
-// use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, Window, WindowOptions};
 
-// pub fn create_window() -> Window {
-//     Window::new(
-//         "rsnes ppu",
-//         SCREEN_WIDTH,
-//         SCREEN_HEIGHT,
-//         WindowOptions {
-//             resize: false,
-//             ..WindowOptions::default()
-//         },
-//     )
-//     .expect("[ERR::WindowInit] Unable to create display context.")
-// }
+pub fn create_window() -> Window {
+    Window::new(
+        "rsnes ppu",
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        WindowOptions {
+            resize: false,
+            ..WindowOptions::default()
+        },
+    )
+    .expect("[ERR::WindowInit] Unable to create display context.")
+}
 
-// pub fn update_window(window: &mut Window, framebuffer: &Vec<u32>) {
-//     window
-//         .update_with_buffer(framebuffer, WIDTH, HEIGHT)
-//         .expect("[ERR::Render] Framebuffer refused to cooperate.");
-// }
+pub fn update_window(window: &mut Window, framebuffer: &Vec<u32>) {
+    window
+        .update_with_buffer(framebuffer, WIDTH, HEIGHT)
+        .expect("[ERR::Render] Framebuffer refused to cooperate.");
+}
 
 fn main() {
     let (tiles, image_width) = load_and_split_image("./tileset.png");
@@ -36,11 +36,11 @@ fn main() {
 
     let tiles_per_row = image_width / TILE_SIZE as usize;
 
-    // let mut window = create_window();
+    let mut window = create_window();
 
-    // // hard-coded display => to be removed (but ok for pr #13)
-    // while window.is_open() && !window.is_key_down(Key::Escape) {
-    //     ppu.render(tiles_per_row);
-    //     update_window(&mut window, &ppu.framebuffer);
-    // }
+    // hard-coded display => to be removed (but ok for pr #13)
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        ppu.render(tiles_per_row);
+        update_window(&mut window, &ppu.framebuffer);
+    }
 }
