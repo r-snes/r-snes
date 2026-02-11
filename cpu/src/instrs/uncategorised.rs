7cb6e70de99fad7b26aafb3b25acef1a0e664d4f
Florent Charpentier <florent.charpentier@epitech.eu>
2026-02-11 23:46:05 +1100

    :sparkles: Add arithmetic RMW instructions

--- cpu/src/instrs/uncategorised.rs
+++ cpu/src/instrs/uncategorised.rs
@@ -4,19 +4,6 @@
 
 use instr_metalang_procmacro::cpu_instr;
 
-// `INX` instruction: increment register X
-//
-// Flags set:
-// - `Z`: whether the result is zero
-// - `N`: whether the result is negative (if it were interpreted as signed)
-cpu_instr!(inx {
-    cpu.registers.X = cpu.registers.X.wrapping_add(1);
-    cpu.registers.P.Z = cpu.registers.X == 0;
-    cpu.registers.P.N = cpu.registers.X > 0x7fff;
-
-    meta END_CYCLE Internal;
-});
-
 // `NOP`: "no-op" (no operation). Literally does nothing
 cpu_instr!(nop {
     meta END_CYCLE Internal;
