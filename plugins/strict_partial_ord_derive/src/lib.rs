a0ecc9da96455bfb028590def53c2a450685a170
Florent Charpentier <florent.charpentier@epitech.eu>
2026-03-29 04:21:02 +1100

    :white_check_mark: Add macro coverage for strict_partial_ord

--- plugins/strict_partial_ord_derive/src/lib.rs
+++ plugins/strict_partial_ord_derive/src/lib.rs
@@ -1,8 +1,15 @@
-use proc_macro::TokenStream;
-use quote::format_ident;
-use quote::quote;
-use syn::parse_macro_input;
-use syn::DeriveInput;
+use {
+    proc_macro::TokenStream,
+    quote::{
+        ToTokens,
+        format_ident,
+        quote,
+    },
+    syn::{
+        ItemStruct,
+        parse::{self, Parse, ParseStream},
+    }
+};
 
 /// Custom derive macro which implements [`std::cmp::PartialOrd`]
 ///
@@ -34,41 +41,79 @@ use syn::DeriveInput;
 /// - Only in the occassion all fields evaluate equal will the structs be recognised equal.
 #[proc_macro_derive(PartialOrd)]
 pub fn strict_partial_ord(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let item: syn::Item = input.into();
-    let syn::Item::Struct(item_struct) = item else {
-        panic!("Strict PartialOrd only support struct inputs");
-    };
-
-    derive_partial_ord(&item_struct).into()
+    derive_partial_ord(input.into()).into()
 }
 
-fn derive_partial_ord(input: &syn::ItemStruct) -> proc_macro2::TokenStream {
-    let ident = &input.ident;
-    let members = input.fields.members();
-    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+fn derive_partial_ord(input: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+    // Parse the annotated item.
+    let ast: StrictPartialOrd = match syn::parse2(input) {
+        Ok(parsed) => parsed,
+        Err(e) => return e.into_compile_error()
+    };
 
-    let acc_varname = format_ident!("{}", "acc");
-    let members_match_blocks = members.map(|member| {
-        quote! {
-            let member_ord = self.#member.partial_cmp(&other.#member)?;
-            match (#acc_varname, member_ord) {
-                (std::cmp::Ordering::Equal, x) => #acc_varname = x,
-                (std::cmp::Ordering::Less, std::cmp::Ordering::Less) => (),
-                (std::cmp::Ordering::Greater, std::cmp::Ordering::Greater) => (),
-                _ => return None,
-            };
-        }
-    });
-    quote! {
-        impl #impl_generics std::cmp::PartialOrd for #ident #ty_generics #where_clause {
-            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-                let mut #acc_varname = std::cmp::Ordering::Equal;
+    // Return the macro's expanded form (the main logic is in `Pod::to_tokens`).
+    let mut ts = proc_macro2::TokenStream::new();
+    ast.to_tokens(&mut ts);
+    ts
+}
 
-                #(#members_match_blocks)*
+struct StrictPartialOrd {
+    item: ItemStruct,
+}
 
-                Some(#acc_varname)
+impl Parse for StrictPartialOrd {
+    fn parse(input: ParseStream) -> parse::Result<Self> {
+        Ok(Self { item: input.call(ItemStruct::parse)? })
+    }
+}
+
+impl ToTokens for StrictPartialOrd {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let ident = &self.item.ident;
+        let members = self.item.fields.members();
+        let (impl_generics, ty_generics, where_clause) = self.item.generics.split_for_impl();
+
+        let acc_varname = format_ident!("{}", "acc");
+        let members_match_blocks = members.map(|member| {
+            quote! {
+                let member_ord = self.#member.partial_cmp(&other.#member)?;
+                match (#acc_varname, member_ord) {
+                    (std::cmp::Ordering::Equal, x) => #acc_varname = x,
+                    (std::cmp::Ordering::Less, std::cmp::Ordering::Less) => (),
+                    (std::cmp::Ordering::Greater, std::cmp::Ordering::Greater) => (),
+                    _ => return None,
+                };
             }
-        }
+        });
+
+        tokens.extend(quote!(
+            impl #impl_generics std::cmp::PartialOrd for #ident #ty_generics #where_clause {
+                fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+                    let mut #acc_varname = std::cmp::Ordering::Equal;
+
+                    #(#members_match_blocks)*
+
+                    Some(#acc_varname)
+                }
+            }
+        ));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use runtime_macros::emulate_derive_macro_expansion;
+    use super::derive_partial_ord;
+    use std::{env, fs};
+
+    #[test]
+    fn code_coverage() {
+        // This code doesn't check much. Instead, it does macro expansion at run time to let
+        // tarpaulin measure code coverage for the macro.
+        let mut path = env::current_dir().unwrap();
+        path.push("tests");
+        path.push("lib.rs");
+        let file = fs::File::open(path).unwrap();
+        emulate_derive_macro_expansion(file, &[("strict::PartialOrd", derive_partial_ord)]).unwrap();
     }
 }
