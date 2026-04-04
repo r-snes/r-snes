4ced4b8699f31bdce07dbabd380429bd24600ad5
Clément Baziret <clement.baziret@epitech.eu>
2026-04-04 11:42:57 +0200

    :recycle::white_check_mark: Rework `Bus` units tests to correspond to new function signature

--- bus/src/bus.rs
+++ bus/src/bus.rs
@@ -46,77 +46,91 @@ impl Bus {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::rom::test_rom::*;
-//     use common::snes_address::snes_addr;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::rom::test_rom::*;
+    use common::snes_address::snes_addr;
 
-//     #[test]
-//     fn test_wram_read_write_through_bus() {
-//         let rom_data = create_valid_lorom(0x20000);
-//         let (rom_path, _dir) = create_temp_rom(&rom_data);
-//         let mut bus = Bus::new(&rom_path).unwrap();
+    fn init_extern_components() -> (CPU, PPU, Apu) {
+        let cpu = CPU::poweron();
+        let ppu = PPU::new();
+        let apu = Apu::new();
 
-//         let addr = snes_addr!(0:0x0010);
-//         bus.write(addr, 0x42);
-//         assert_eq!(bus.read(addr), 0x42);
+        (cpu, ppu, apu)
+    }
 
-//         let addr_mirror = snes_addr!(0x80:0x0010);
-//         assert_eq!(bus.read(addr), 0x42);
-//         assert_eq!(bus.read(addr_mirror), 0x42);
+    #[test]
+    fn test_wram_read_write_through_bus() {
+        let (mut cpu, mut ppu, mut apu) = init_extern_components();
+        let rom_data = create_valid_lorom(0x20000);
+        let (rom_path, _dir) = create_temp_rom(&rom_data);
+        let mut bus = Bus::new(&rom_path).unwrap();
 
-//         let real_addr = snes_addr!(0x7E:0x0010);
-//         assert_eq!(bus.read(real_addr), 0x42);
+        let addr = snes_addr!(0:0x0010);
+        bus.write(addr, 0x42, &mut cpu, &mut ppu, &mut apu);
+        assert_eq!(bus.read(addr, &mut cpu, &mut ppu, &mut apu), 0x42);
 
-//         bus.write(real_addr, 0x21);
-//         assert_eq!(bus.read(real_addr), 0x21);
-//         assert_eq!(bus.read(addr), 0x21);
-//         assert_eq!(bus.read(addr_mirror), 0x21);
-//     }
+        let addr_mirror = snes_addr!(0x80:0x0010);
+        assert_eq!(bus.read(addr, &mut cpu, &mut ppu, &mut apu), 0x42);
+        assert_eq!(bus.read(addr_mirror, &mut cpu, &mut ppu, &mut apu), 0x42);
 
-//     #[test]
-//     fn test_io_read_write_through_bus() {
-//         let rom_data = create_valid_lorom(0x20000);
-//         let (rom_path, _dir) = create_temp_rom(&rom_data);
-//         let mut bus = Bus::new(&rom_path).unwrap();
+        let real_addr = snes_addr!(0x7E:0x0010);
+        assert_eq!(bus.read(real_addr, &mut cpu, &mut ppu, &mut apu), 0x42);
 
-//         let addr = snes_addr!(0:0x2345);
-//         bus.write(addr, 0x77);
-//         assert_eq!(bus.read(addr), 0x77);
+        bus.write(real_addr, 0x21, &mut cpu, &mut ppu, &mut apu);
+        assert_eq!(bus.read(real_addr, &mut cpu, &mut ppu, &mut apu), 0x21);
+        assert_eq!(bus.read(addr, &mut cpu, &mut ppu, &mut apu), 0x21);
+        assert_eq!(bus.read(addr_mirror, &mut cpu, &mut ppu, &mut apu), 0x21);
+    }
 
-//         let addr_mirror = snes_addr!(0x9E:0x2345);
-//         assert_eq!(bus.read(addr), 0x77);
-//         assert_eq!(bus.read(addr_mirror), 0x77);
-//     }
+    #[test]
+    fn test_io_read_write_through_bus() {
+        let (mut cpu, mut ppu, mut apu) = init_extern_components();
+        let rom_data = create_valid_lorom(0x20000);
+        let (rom_path, _dir) = create_temp_rom(&rom_data);
+        let mut bus = Bus::new(&rom_path).unwrap();
 
-//     #[test]
-//     fn test_rom_read_write_through_bus() {
-//         let mut rom_data = create_valid_lorom(0x100000 * 0x40);
-//         rom_data[0x0001] = 0x42;
-//         let (rom_path, _dir) = create_temp_rom(&rom_data);
-//         let mut bus = Bus::new(&rom_path).unwrap();
+        cpu.data_bus = 0x20;
+        let addr = snes_addr!(0:0x5000);
+        let read_value = bus.read(addr, &mut cpu, &mut ppu, &mut apu);
+        assert_eq!(read_value, 0x20);
 
-//         let addr = snes_addr!(0:0x8001);
-//         assert_eq!(bus.read(addr), 0x42);
-//         bus.write(addr, 0x21);
-//         assert_eq!(bus.read(addr), 0x42);
+        bus.write(addr, 0x40, &mut cpu, &mut ppu, &mut apu);
+        let read_value = bus.read(addr, &mut cpu, &mut ppu, &mut apu);
+        assert_eq!(read_value, 0x20);
+    }
 
-//         let other_addr = snes_addr!(0x40:0x8001);
-//         assert_eq!(bus.read(other_addr), 0);
-//         bus.write(other_addr, 0x21);
-//         assert_eq!(bus.read(other_addr), 0);
-//     }
+    #[test]
+    fn test_rom_read_write_through_bus() {
+        let (mut cpu, mut ppu, mut apu) = init_extern_components();
+        let mut rom_data = create_valid_lorom(0x100000 * 0x40);
+        rom_data[0x0001] = 0x42;
+        let (rom_path, _dir) = create_temp_rom(&rom_data);
+        let mut bus = Bus::new(&rom_path).unwrap();
 
-//     #[test]
-//     #[should_panic(expected = "ERROR: Couldn't extract value from ROM")]
-//     fn test_rom_read_out_of_range_panics() {
-//         let rom_data = create_valid_lorom(0x20000);
-//         let (rom_path, _dir) = create_temp_rom(&rom_data);
-//         let bus = Bus::new(&rom_path).unwrap();
+        let addr = snes_addr!(0:0x8001);
+        assert_eq!(bus.read(addr, &mut cpu, &mut ppu, &mut apu), 0x42);
+        bus.write(addr, 0x21, &mut cpu, &mut ppu, &mut apu);
+        assert_eq!(bus.read(addr, &mut cpu, &mut ppu, &mut apu), 0x42);
 
-//         // Create an address mapped to an offset beyond the 128 KiB dummy ROM.
-//         let addr = snes_addr!(0x7D:0xFFFF);
-//         bus.rom.read(addr);
-//     }
-// }
+        let other_addr = snes_addr!(0x40:0x8001);
+        assert_eq!(bus.read(other_addr, &mut cpu, &mut ppu, &mut apu), 0);
+        bus.write(other_addr, 0x21, &mut cpu, &mut ppu, &mut apu);
+        assert_eq!(bus.read(other_addr, &mut cpu, &mut ppu, &mut apu), 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "ERROR: Couldn't extract value from ROM")]
+    fn test_rom_read_out_of_range_panics() {
+        let (mut cpu, mut ppu, mut apu) = init_extern_components();
+        let rom_data = create_valid_lorom(0x20000);
+        let (rom_path, _dir) = create_temp_rom(&rom_data);
+        let mut bus = Bus::new(&rom_path).unwrap();
+
+        // Create an address mapped to an offset beyond the 128 KiB dummy ROM.
+        let addr = snes_addr!(0x7D:0xFFFF);
+        bus.read(addr, &mut cpu, &mut ppu, &mut apu);
+        // bus.rom.read(addr);
+    }
+}
