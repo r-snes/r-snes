3237b2b833676740ad0197b335d3ca2776db4eae
Florent Charpentier <florent.charpentier@epitech.eu>
2026-05-31 00:40:43 +1000

    :sparkles: Implement BRK, COP, RTI, STP
    very rough implementation which at least allows us to move further in
    the test ROM (still much better than no implementation at all!)

---
+++ cpu/src/instrs/interrupts.rs
@@ -0,0 +1,67 @@
+use instr_metalang_procmacro::cpu_instr_no_inc_pc;
+use duplicate::duplicate;
+
+cpu_instr_no_inc_pc!(brk {
+    meta FETCH8_IMM; // ignored imm read
+
+    if cpu.registers.E {
+        // skip the PB push if in emu mode
+        return brk_cyc3(cpu);
+    }
+    meta PUSH8 cpu.registers.PB;
+    meta PUSH16 cpu.registers.PC.wrapping_add(2);
+    meta PUSH8 cpu.registers.P.into();
+
+    cpu.registers.P.I = true;
+    cpu.registers.P.D = false;
+
+    let addr = if cpu.registers.E {
+        0xFFFE
+    } else {
+        0xFFE6
+    };
+    cpu.addr_bus = snes_addr!(0:addr);
+    meta FETCH16_INTO cpu.registers.PC;
+    cpu.registers.PB = 0;
+});
+
+cpu_instr_no_inc_pc!(cop {
+    meta FETCH8_IMM; // ignored imm read
+
+    if cpu.registers.E {
+        // skip the PB push if in emu mode
+        return cop_cyc3(cpu);
+    }
+    meta PUSH8 cpu.registers.PB;
+    meta PUSH16 cpu.registers.PC.wrapping_add(2);
+    meta PUSH8 cpu.registers.P.into();
+
+    cpu.registers.P.I = true;
+    cpu.registers.P.D = false;
+
+    let addr = if cpu.registers.E {
+        0xFFF4
+    } else {
+        0xFFE4
+    };
+    cpu.addr_bus = snes_addr!(0:addr);
+    meta FETCH16_INTO cpu.registers.PC;
+    cpu.registers.PB = 0;
+});
+
+cpu_instr_no_inc_pc!(rti {
+    meta END_CYCLE Internal;
+    meta END_CYCLE Internal;
+
+    meta PULL8;
+    cpu.registers.P = cpu.data_bus.into();
+
+    meta PULL16_INTO cpu.registers.PC;
+
+    // emu mode doesn't pull PB, goes straight to opcode fetch
+    if cpu.registers.E {
+        return opcode_fetch(cpu);
+    }
+
+    meta PULL8_INTO cpu.registers.PB;
+});
