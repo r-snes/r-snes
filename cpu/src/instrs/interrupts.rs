b7cf784e9cc11096088804211142d9cb775d7e12
Florent Charpentier <florent.charpentier@epitech.eu>
2026-05-31 00:43:04 +1000

    :white_check_mark: Test BRK in emu and native modes

--- cpu/src/instrs/interrupts.rs
+++ cpu/src/instrs/interrupts.rs
@@ -65,3 +65,138 @@ cpu_instr_no_inc_pc!(rti {
 
     meta PULL8_INTO cpu.registers.PB;
 });
+
+#[cfg(test)]
+mod test {
+    use super::super::test_prelude::*;
+
+    #[test]
+    fn brk_emu() {
+        let mut regs = Registers::default();
+        regs.PB = 0x12;
+        regs.PC = 0x3456;
+        regs.S = 0x0180;
+        regs.P = 0b10101010.into();
+        regs.E = true;
+
+        let mut expected_regs = regs.clone();
+
+        let mut cpu = CPU::new(regs);
+
+        expect_opcode_fetch(&mut cpu, 0x00);
+        expect_read_cycle(
+            &mut cpu,
+            snes_addr!(0x12:0x3457),
+            0x33,
+            "signature byte (ignored)",
+        );
+        expect_write_cycle(
+            &mut cpu,
+            snes_addr!(0:0x0180),
+            0x34,
+            "PCH",
+        );
+        expect_write_cycle(
+            &mut cpu,
+            snes_addr!(0:0x017f),
+            0x56 + 2, // pushes PC + 2
+            "PCL",
+        );
+        expect_write_cycle(
+            &mut cpu,
+            snes_addr!(0:0x017e),
+            0b10101010,
+            "P",
+        );
+
+        expect_read_cycle(
+            &mut cpu,
+            snes_addr!(0:0xFFFE),
+            0x66,
+            "interrupt routine addr low",
+        );
+        expect_read_cycle(
+            &mut cpu,
+            snes_addr!(0:0xFFFF),
+            0x33,
+            "interrupt routine addr hi",
+        );
+
+        expect_opcode_fetch_cycle(&mut cpu);
+
+        expected_regs.PC = 0x3366;
+        expected_regs.PB = 0;
+        expected_regs.S = 0x017d;
+        expected_regs.P.D = false;
+        expected_regs.P.I = true;
+        assert_eq!(*cpu.regs(), expected_regs);
+    }
+
+    #[test]
+    fn brk_nat() {
+        let mut regs = Registers::default();
+        regs.PB = 0x12;
+        regs.PC = 0x3456;
+        regs.S = 0x0180;
+        regs.P = 0b10101010.into();
+        regs.E = false;
+
+        let mut expected_regs = regs.clone();
+
+        let mut cpu = CPU::new(regs);
+
+        expect_opcode_fetch(&mut cpu, 0x00);
+        expect_read_cycle(
+            &mut cpu,
+            snes_addr!(0x12:0x3457),
+            0x33,
+            "signature byte (ignored)",
+        );
+        expect_write_cycle(
+            &mut cpu,
+            snes_addr!(0:0x0180),
+            0x12,
+            "PB",
+        );
+        expect_write_cycle(
+            &mut cpu,
+            snes_addr!(0:0x017f),
+            0x34,
+            "PCH",
+        );
+        expect_write_cycle(
+            &mut cpu,
+            snes_addr!(0:0x017e),
+            0x56 + 2, // pushes PC + 2
+            "PCL",
+        );
+        expect_write_cycle(
+            &mut cpu,
+            snes_addr!(0:0x017d),
+            0b10101010,
+            "P",
+        );
+
+        expect_read_cycle(
+            &mut cpu,
+            snes_addr!(0:0xFFE6),
+            0x66,
+            "interrupt routine addr low",
+        );
+        expect_read_cycle(
+            &mut cpu,
+            snes_addr!(0:0xFFE7),
+            0x33,
+            "interrupt routine addr hi",
+        );
+
+        expect_opcode_fetch_cycle(&mut cpu);
+
+        expected_regs.PC = 0x3366;
+        expected_regs.PB = 0;
+        expected_regs.S = 0x017c;
+        expected_regs.P.D = false;
+        expected_regs.P.I = true;
+        assert_eq!(*cpu.regs(), expected_regs);
+    }
+}
