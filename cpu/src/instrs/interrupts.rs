6ab06b1dfb663d7fe7d7e29674090f7e4bf165ac
Florent Charpentier <florent.charpentier@epitech.eu>
2026-06-14 02:25:12 +1000

    :recycle: De-duplicate implementation of BRK and COP

--- cpu/src/instrs/interrupts.rs
+++ cpu/src/instrs/interrupts.rs
@@ -1,53 +1,36 @@
 use instr_metalang_procmacro::cpu_instr_no_inc_pc;
 use duplicate::duplicate;
 
-cpu_instr_no_inc_pc!(brk {
-    meta FETCH8_IMM; // ignored imm read
+duplicate! {
+    [
+        DUP_name    DUP_cyc3    DUP_emu_vec     DUP_nat_vec;
+        [brk]       [brk_cyc3]  [0xFFFE]        [0xFFE6];
+        [cop]       [cop_cyc3]  [0xFFF4]        [0xFFE4];
+    ]
+    cpu_instr_no_inc_pc!(DUP_name {
+        meta FETCH8_IMM; // ignored imm read
 
-    if cpu.registers.E {
-        // skip the PB push if in emu mode
-        return brk_cyc3(cpu);
-    }
-    meta PUSH8 cpu.registers.PB;
-    meta PUSH16 cpu.registers.PC.wrapping_add(2);
-    meta PUSH8 cpu.registers.P.into();
+        if cpu.registers.E {
+            // skip the PB push if in emu mode
+            return DUP_cyc3(cpu);
+        }
+        meta PUSH8 cpu.registers.PB;
+        meta PUSH16 cpu.registers.PC.wrapping_add(2);
+        meta PUSH8 cpu.registers.P.into();
 
-    cpu.registers.P.I = true;
-    cpu.registers.P.D = false;
+        cpu.registers.P.I = true;
+        cpu.registers.P.D = false;
 
-    let addr = if cpu.registers.E {
-        0xFFFE
-    } else {
-        0xFFE6
-    };
-    cpu.addr_bus = snes_addr!(0:addr);
-    meta FETCH16_INTO cpu.registers.PC;
-    cpu.registers.PB = 0;
-});
-
-cpu_instr_no_inc_pc!(cop {
-    meta FETCH8_IMM; // ignored imm read
-
-    if cpu.registers.E {
-        // skip the PB push if in emu mode
-        return cop_cyc3(cpu);
-    }
-    meta PUSH8 cpu.registers.PB;
-    meta PUSH16 cpu.registers.PC.wrapping_add(2);
-    meta PUSH8 cpu.registers.P.into();
-
-    cpu.registers.P.I = true;
-    cpu.registers.P.D = false;
-
-    let addr = if cpu.registers.E {
-        0xFFF4
-    } else {
-        0xFFE4
-    };
-    cpu.addr_bus = snes_addr!(0:addr);
-    meta FETCH16_INTO cpu.registers.PC;
-    cpu.registers.PB = 0;
-});
+        let addr = if cpu.registers.E {
+            DUP_emu_vec
+        } else {
+            DUP_nat_vec
+        };
+        cpu.addr_bus = snes_addr!(0:addr);
+        meta FETCH16_INTO cpu.registers.PC;
+        cpu.registers.PB = 0;
+    });
+}
 
 cpu_instr_no_inc_pc!(rti {
     meta END_CYCLE Internal;
