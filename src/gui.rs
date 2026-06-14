bf18ab4f355ad0b8df96b5c4f8ca09189f36c240
Florent Charpentier <florent.charpentier@epitech.eu>
2026-06-15 00:55:22 +1000

    PR #67

--- src/gui.rs
+++ src/gui.rs
@@ -3,23 +3,34 @@ use std::path::PathBuf;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
+use ppu::constants::{SCREEN_HEIGHT, SCREEN_WIDTH};
+
 pub struct Gui {
     _sdl_ctx: sdl2::Sdl,
     canvas: sdl2::render::Canvas<sdl2::video::Window>,
     event_pump: sdl2::EventPump,
-    framebuffer: Vec<u8>,
 }
 
+#[derive(PartialEq, Eq)]
 pub enum RSnesEvent {
+    /// Load a new ROM, showing a file picker (closes current game)
     LoadRom { path: PathBuf },
+
+    /// Close the currently open game (or quit if no game open)
+    Close,
+
+    /// Quit the emulator program altogether
     Quit,
+
+    /// An key mapped to an emulated button has been pressed
+    ButtonDown,
+
+    /// An key mapped to an emulated button has been released
+    ButtonUp,
 }
 
 #[cfg(not(tarpaulin_include))]
 impl Gui {
-    pub const SNES_WIDTH: usize = 256; // TODO : Remove when GUI linked with PPU
-    pub const SNES_HEIGHT: usize = 224; // TODO : Remove when GUI linked with PPU
-
     pub const FRAME_RATE: u16 = 60;
     pub const FRAME_DURATION: f64 = 1.0 / Self::FRAME_RATE as f64;
 
@@ -28,7 +39,7 @@ impl Gui {
         let video_subsystem = sdl_ctx.video()?;
 
         let window = video_subsystem
-            .window("R-SNES", 1920 / 2, 1080 / 2)
+            .window("R-SNES", SCREEN_WIDTH as u32 * 2, SCREEN_HEIGHT as u32 * 2)
             .position_centered()
             .build()
             .map_err(|e| e.to_string())?;
@@ -45,28 +56,7 @@ impl Gui {
             _sdl_ctx: sdl_ctx,
             canvas,
             event_pump,
-            framebuffer: Self::temporary_framebuffer(),
         })
-    }
-
-    pub fn temporary_framebuffer() -> Vec<u8> {
-        let mut framebuffer = vec![0u8; Self::SNES_WIDTH * Self::SNES_HEIGHT * 4];
-
-        for y in 0..Self::SNES_HEIGHT {
-            for x in 0..Self::SNES_WIDTH {
-                let pixel_index = y * Self::SNES_WIDTH + x;
-                let byte_index = pixel_index * 4;
-
-                let shade = ((x + y) & 0xFF) as u8;
-
-                framebuffer[byte_index + 0] = shade; // B
-                framebuffer[byte_index + 1] = shade; // G
-                framebuffer[byte_index + 2] = shade; // R
-                framebuffer[byte_index + 3] = 255; // A
-            }
-        }
-
-        framebuffer
     }
 
     pub fn clear(&mut self, r: u8, g: u8, b: u8) {
@@ -79,41 +69,85 @@ impl Gui {
         self.canvas.present();
     }
 
-    fn handle_events(&mut self) -> impl Iterator<Item = RSnesEvent> {
-        self.event_pump
-            .poll_iter()
-            .filter_map(|event: Event| match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => Some(RSnesEvent::Quit),
-                Event::KeyDown {
-                    keycode: Some(Keycode::L),
-                    ..
-                } => match rfd::FileDialog::new().pick_file() {
+    fn map_event(event: sdl2::event::Event) -> Option<RSnesEvent> {
+        use sdl2::keyboard::Mod;
+
+        match event {
+            Event::Quit { .. } => Some(RSnesEvent::Quit),
+            Event::KeyDown {
+                keycode: Some(Keycode::Q),
+                keymod,
+                ..
+            } if keymod.intersects(Mod::LCTRLMOD | Mod::RCTRLMOD) => Some(RSnesEvent::Quit),
+
+            Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                repeat: false,
+                ..
+            } => Some(RSnesEvent::Close),
+
+            Event::KeyDown {
+                keycode: Some(Keycode::L),
+                keymod,
+                ..
+            } if keymod.intersects(Mod::LCTRLMOD | Mod::RCTRLMOD) => {
+                match rfd::FileDialog::new().pick_file() {
                     Some(path) => Some(RSnesEvent::LoadRom { path }),
                     None => None,
-                },
-                _ => None,
-            })
+                }
+            }
+
+            Event::KeyDown {
+                keycode: Some(Keycode::Space),
+                repeat: false,
+                keymod,
+                ..
+            } if !keymod
+                .intersects(Mod::LCTRLMOD | Mod::RCTRLMOD | Mod::LALTMOD | Mod::RALTMOD) =>
+            {
+                Some(RSnesEvent::ButtonDown)
+            }
+
+            Event::KeyUp {
+                keycode: Some(Keycode::Space),
+                ..
+            } => Some(RSnesEvent::ButtonUp),
+
+            _ => None,
+        }
     }
 
-    fn draw_framebuffer(&mut self) -> Result<(), String> {
+    fn handle_events(&mut self) -> impl Iterator<Item = RSnesEvent> {
+        self.event_pump.poll_iter().filter_map(Self::map_event)
+    }
+
+    pub fn wait_for_event(&mut self) -> RSnesEvent {
+        loop {
+            match Self::map_event(self.event_pump.wait_event()) {
+                Some(e) => return e,
+                None => {}
+            }
+        }
+    }
+
+    pub fn draw_framebuffer(
+        &mut self,
+        framebuffer: &ppu::rendering::RawFramebuffer,
+    ) -> Result<(), String> {
         use sdl2::pixels::PixelFormatEnum;
 
         let texture_creator = self.canvas.texture_creator();
 
         let mut texture = texture_creator
             .create_texture_streaming(
-                PixelFormatEnum::ARGB8888,
-                Self::SNES_WIDTH as u32,
-                Self::SNES_HEIGHT as u32,
+                PixelFormatEnum::RGB24,
+                SCREEN_WIDTH as u32,
+                SCREEN_HEIGHT as u32,
             )
             .map_err(|e| e.to_string())?;
 
         texture
-            .update(None, &self.framebuffer, Self::SNES_WIDTH * 4)
+            .update(None, framebuffer, SCREEN_WIDTH * 3)
             .map_err(|e| e.to_string())?;
 
         self.canvas.copy(&texture, None, None)?;
@@ -121,9 +155,12 @@ impl Gui {
         Ok(())
     }
 
-    pub fn update(&mut self) -> impl Iterator<Item = RSnesEvent> {
+    pub fn update(
+        &mut self,
+        framebuffer: &ppu::rendering::RawFramebuffer,
+    ) -> impl Iterator<Item = RSnesEvent> + use<'_> {
         self.clear(30, 30, 35);
-        let _ = self.draw_framebuffer(); // TODO: Handle error properly
+        let _ = self.draw_framebuffer(framebuffer); // TODO: Handle error properly
         self.present();
 
         self.handle_events() // Handle events after presenting window because it's borrowing mut self
