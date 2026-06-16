5d1d1eca40c64164004d0c675954068562557a02
Florent Charpentier <florent.charpentier@epitech.eu>
2026-06-17 05:07:26 +1000

    more?

--- plugins/plugin.lua
+++ plugins/plugin.lua
@@ -2,17 +2,17 @@ return {
     permissions = "all",
     init = function()
         print("loaded rsnes? type: " .. type(rsnes))
-        print("loaded regs? type: " .. type(regs))
+        print("loaded regs? type: " .. type(rsnes.cpu))
 
-        print("plugin: addrbus is currently " .. regs.bus_bank .. ":" .. regs.bus_addr)
-        print("plugin: PB:PC is currently " .. regs.pb .. ":" .. regs.pc)
-        regs.pb = 0xaa
-        print("plugin: PB:PC is currently " .. regs.pb .. ":" .. regs.pc)
+        print("plugin: addrbus is currently " .. rsnes.cpu.bus_bank .. ":" .. rsnes.cpu.bus_addr)
+        print("plugin: PB:PC is currently " .. rsnes.cpu.pb .. ":" .. rsnes.cpu.pc)
+        rsnes.cpu.pb = 0xaa
+        print("plugin: PB:PC is currently " .. rsnes.cpu.pb .. ":" .. rsnes.cpu.pc)
     end,
 
     actions = {
         default = function()
-            print("ran plugin default action")
+            print("ran plugin default action, addr: " .. rsnes.cpu.bus_addr)
         end,
     }
 }
