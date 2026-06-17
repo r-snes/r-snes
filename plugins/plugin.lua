4b65380df1d524015bab1b9eb4dfef8c9c0dd7e6
Florent Charpentier <florent.charpentier@epitech.eu>
2026-06-18 03:01:35 +1000

    :sparkles: Test counting plugins

--- plugins/plugin.lua
+++ plugins/plugin.lua
@@ -8,11 +8,62 @@ return {
         print("plugin: PB:PC is currently " .. rsnes.cpu.pb .. ":" .. rsnes.cpu.pc)
         rsnes.cpu.pb = 0xaa
         print("plugin: PB:PC is currently " .. rsnes.cpu.pb .. ":" .. rsnes.cpu.pc)
+
+        instrs = {}
+
+        test_results = {}
+        test_idx = 0
     end,
 
     actions = {
         default = function()
-            print("ran plugin default action, addr: " .. rsnes.cpu.bus_addr)
+            -- print("program so far:")
+
+            -- for addr,data in pairs(instrs) do
+            --     print(addr, data[1] .. "×" .. data[2])
+            -- end
+
+            print("test results:")
+            local ok=0
+            local ko=0
+
+            for i=0,(test_idx-1) do
+                local res = test_results[i]
+                print(i, res)
+                if (res) then
+                    ok = ok + 1
+                else
+                    ko = ko + 1
+                end
+            end
+            print("OK: " .. ok .. ", KO: " .. ko)
         end,
-    }
+    },
+
+    autoactions = {
+        on_instr = function(opcode, pb, pc)
+            -- -- we're never interested in anything where pb is not 0
+            -- if (pb ~= 0) then return end
+
+            -- init_test addr: start of a test mark as success by default
+            if (pc == 0x8132) then
+                test_results[test_idx] = true
+                test_idx = test_idx + 1
+                return
+            end
+            -- fail addr: test completed and failed, mark as failed
+            if (pc == 0x81AE) then
+                test_results[test_idx - 1] = false
+                return
+            end
+
+            -- local addr = (pb << 16) + pc
+
+            -- if (instrs[addr] == nil) then
+            --     instrs[addr] = { opcode, 1 }
+            -- else
+            --     instrs[addr][2] = instrs[addr][2] + 1
+            -- end
+        end,
+    },
 }
