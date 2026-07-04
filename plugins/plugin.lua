34151539689324595b26cf8e3c183a0943909ed6
Florent Charpentier <114689807+C0Florent@users.noreply.github.com>
2026-07-04 18:33:20 +0200

    :sparkles: plugins: Run lua code (#69)
    * :sparkles: Get plugin init function and run it in example
    
    * :recycle: Add helper to run a stashed lua function
    
    * :recycle: Add specialised helper to run the plugin init function
    
    * :recycle: Refactor plugin table parsing
    
    * :sparkles: Add plugin default action
    
    * :white_check_mark: Add basic test for plugin function execution

--- plugins/plugin.lua
+++ plugins/plugin.lua
@@ -1,3 +1,14 @@
 return {
-    permissions = "all"
+    permissions = "all",
+    init = function()
+        i = 10
+        print("plugin initialised: i =", i)
+    end,
+
+    actions = {
+        default = function()
+            i = i + 1
+            print("ran plugin default action, i =", i)
+        end,
+    }
 }
