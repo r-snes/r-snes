04df4e973b367ee9174950f3f4217712b1f72c2f
Florent Charpentier <florent.charpentier@epitech.eu>
2026-07-12 16:38:42 +0200

    :lipstick: Put perm req in scrollable in example

--- plugins/src/bin/main.rs
+++ plugins/src/bin/main.rs
@@ -1,3 +1,4 @@
+use egui::ScrollArea;
 use plugins::plugin::{
     Plugin,
     gui::PluginPermRequest,
@@ -21,8 +22,10 @@ impl<'app, 'req> PermRequestApp<'app, 'req> {
 impl eframe::App for PermRequestApp<'_, '_> {
     fn ui(&mut self, ui: &mut egui::Ui, _: &mut eframe::Frame) {
         egui::CentralPanel::default().show_inside(ui, |ui| {
-            ui.heading(&self.title);
-            self.perm_request.show_gui(ui);
+            ScrollArea::both().show(ui, |ui| {
+                ui.heading(&self.title);
+                self.perm_request.show_gui(ui);
+            });
         });
     }
 }
