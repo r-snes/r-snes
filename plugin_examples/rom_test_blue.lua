e4a6d006fc1e2202357998b35e1e38b7e3dce5cb
Florent Charpentier <florent.charpentier@epitech.eu>
2026-07-15 21:41:36 +0200

    :sparkles: Improve ppu plugin module showcase

--- plugin_examples/rom_test_blue.lua
+++ plugin_examples/rom_test_blue.lua
@@ -1,3 +1,26 @@
+-- CGRAM colours are in BGR555, with red in least significant
+-- bits, and blue in most signficant bits
+colors = {
+    {
+        -- (B)01000 (G)00000 (B)00000 = 8192: dark blue
+        bg = 8192,
+        -- (B)11111 (G)11000 (B)11000 = 32536: light blue
+        fg = 32536,
+    },
+    {
+        -- (B)00000 (G)01000 (B)00000 = 8192: dark green
+        bg = 512,
+        -- (B)11000 (G)11111 (B)11000 = 32536: light green
+        fg = 25592,
+    },
+    {
+        -- (B)00000 (G)00000 (B)01000 = 8192: dark red
+        bg = 8,
+        -- (B)11000 (G)11000 (B)11111 = 32536: light red
+        fg = 25375,
+    },
+}
+
 return {
     permissions = {
         internal = {
@@ -5,15 +28,17 @@ return {
         },
     },
 
+    init = function()
+        color_index = 1
+    end,
+
     actions = {
         default = function()
-            -- CGRAM colours are in BGR555, with red in least significant
-            -- bits, and blue in most signficant bits
+            color_index = (color_index + 1)
+            rsnes.ppu.write_cgram(0, colors[color_index].bg)
+            rsnes.ppu.write_cgram(1, colors[color_index].fg)
 
-            -- (B)01000 (G)00000 (B)00000 = 8192: dark blue
-            rsnes.ppu.write_cgram(0, 8192)
-            -- (B)11111 (G)11000 (B)11000 = 32536: light blue
-            rsnes.ppu.write_cgram(1, 32536)
+            color_index = math.fmod(color_index, #colors)
         end,
     },
 }
