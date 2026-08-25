f2ca2b9dbc176a67fa4bf8d07f460cad77b950f9
Clément Baziret <clement.baziret@epitech.eu>
2026-08-25 17:26:28 +0200

    :sparkles: Update ppu for it to have a method callable every master cycles

--- ppu/src/constants.rs
+++ ppu/src/constants.rs
@@ -1,6 +1,12 @@
 pub const VRAM_SIZE: usize = 64 * 1024; // 64 KB
 pub const CGRAM_SIZE: usize = 512; // 512 octets
-pub const SCANLINES_PER_FRAME: u16 = 262;
 
 pub const SCREEN_WIDTH: usize = 256;
 pub const SCREEN_HEIGHT: usize = 224;
+
+pub const MASTER_CYCLES_PER_SCANLINE: u32 = 1364; // NTSC
+pub const DOTS_PER_SCANLINE: u16 = 340;
+pub const SCANLINES_PER_FRAME: u16 = 262; // NTSC
+pub const HBLANK_START_DOT: u16 = 274;
+pub const VBLANK_START_LINE: u16 = 225;
+pub const VBLANK_START_LINE_OVERSCAN: u16 = 240; // SETINI ($2133) bit 2
