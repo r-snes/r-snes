20444d403b15b361eb2b96d24512d39a0d71134a
Clément Baziret <clement.baziret@epitech.eu>
2026-09-04 11:03:09 +0200

    :sparkles: First try at making a faithful dma transfer

--- ppu/src/constants.rs
+++ ppu/src/constants.rs
@@ -5,8 +5,12 @@ pub const SCREEN_WIDTH: usize = 256;
 pub const SCREEN_HEIGHT: usize = 224;
 
 pub const MASTER_CYCLES_PER_SCANLINE: u32 = 1364; // NTSC
+pub const MASTER_CYCLES_SHORT_SCANLINE: u32 = 1360;
 pub const DOTS_PER_SCANLINE: u16 = 340;
 pub const SCANLINES_PER_FRAME: u16 = 262; // NTSC
+
 pub const HBLANK_START_DOT: u16 = 274;
 pub const VBLANK_START_LINE: u16 = 225;
 pub const VBLANK_START_LINE_OVERSCAN: u16 = 240; // SETINI ($2133) bit 2
+
+pub const HDMA_START_DOT: u16 = 278;
