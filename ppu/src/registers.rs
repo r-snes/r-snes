65777fdffe98e6fe166466a7db48e85df41372f7
Tholarr <thomas.cluseau@epitech.eu>
2026-09-11 11:03:09 +0200

    :coffin: Remove obsolete per-layer BG helpers

--- ppu/src/registers.rs
+++ ppu/src/registers.rs
@@ -327,42 +327,6 @@ impl PPURegisters {
         self.bgmode & 0x07
     }
 
-    // Tilemap addresses - bgsc[n] bits[7:2] in 0x400-word steps
-    pub fn bg1_tilemap_addr(&self) -> u16 {
-        (self.bgsc[0] as u16 >> 2) * 0x400
-    }
-
-    pub fn bg2_tilemap_addr(&self) -> u16 {
-        (self.bgsc[1] as u16 >> 2) * 0x400
-    }
-
-    pub fn bg3_tilemap_addr(&self) -> u16 {
-        (self.bgsc[2] as u16 >> 2) * 0x400
-    }
-
-    pub fn bg4_tilemap_addr(&self) -> u16 {
-        (self.bgsc[3] as u16 >> 2) * 0x400
-    }
-
-    // CHR base addresses - BG12NBA low/high nibble, BG34NBA low/high nibble, in 0x1000-word steps
-    pub fn bg1_tiledata_addr(&self) -> u16 {
-        (self.bg12nba as u16 & 0x0F) << 12
-    }
-
-    pub fn bg2_tiledata_addr(&self) -> u16 {
-        (self.bg12nba as u16 >> 4) << 12
-    }
-
-    pub fn bg3_tiledata_addr(&self) -> u16 {
-        (self.bg34nba as u16 & 0x0F) << 12
-    }
-
-    pub fn bg4_tiledata_addr(&self) -> u16 {
-        (self.bg34nba as u16 >> 4) << 12
-    }
-
-    // ------------------------------
-
     pub fn bg_tilemap_addr(&self, bg: usize) -> u16 {
         (self.bgsc[bg] as u16 >> 2) * 0x400
     }
