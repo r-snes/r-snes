e92afc66efe2181d9e25778e2fa4cc9357ef4c08
Tholarr <thomas.cluseau@epitech.eu>
2026-09-16 10:27:23 +0200

    :art: Fix comment's bad indentation

--- ppu/src/registers.rs
+++ ppu/src/registers.rs
@@ -169,7 +169,7 @@ pub struct PPURegisters {
     /// $2131 - CGADSUB (W8)
     pub cgadsub: u8, // Bits: MHBO4321 | Color math operator (M), half (H), backdrop (B), layer enable (O4321)
 
-        /// $2132 - COLDATA (W8)
+    /// $2132 - COLDATA (W8)
     pub coldata: u16, // Bits: BGRCCCCC | Fixed color channel select (BGR) and value (C). Accumulated into a BGR555 fixed color; also the sub-screen backdrop.
 
     /// $2133 - SETINI (W8)
