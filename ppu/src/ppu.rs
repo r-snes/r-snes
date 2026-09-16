8eb0b000949e62d74188f48c2e7b103db6472386
Clément Baziret <clement.baziret@epitech.eu>
2026-09-16 13:36:20 +0200

    :art: Format ppu.rs

--- ppu/src/ppu.rs
+++ ppu/src/ppu.rs
@@ -344,7 +344,6 @@ impl PPU {
 
     /// Non-interlace odd frames shorten scanline 240 to 1360 cycles instead of 1364
     fn scanline_length(&self) -> u32 {
-
         if self.odd_frame() && self.scanline == SHORT_SCANLINE {
             MASTER_CYCLES_SHORT_SCANLINE
         } else {
